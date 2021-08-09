0be020e1c0072b41b65b73ec4c3dd49b06baed69
Jonathan Olvera <jj.olvera.n@gmail.com>
2021-08-09 00:02:15 -0500

    Javascript DOM

---
+++ javascript-module/DOM/script.js
@@ -0,0 +1,62 @@
+// Properties of Car
+// color
+// number seats
+// number of doors
+
+
+//Methods of Car
+// Object can do
+// brake, drive, park
+
+
+//Get Property
+// car.color;
+
+//Set Property
+//car.numberOfDoors = 0 ;
+
+// Call Method
+// car.drive();
+
+
+//Properties
+// innerHTML
+// style
+// firstChild
+
+// Methods
+//.click();
+// .appendChild();
+// .setAttribute();
+
+// By Tag
+// .getElementsByTagName("li");
+// .getElementsByTagName("li")[3].style.color="purple";
+
+// By Class Name
+// .getElementsByClassName("btn");
+// .getElementsByClassName("btn")[0].style.color="red";
+
+// By ID
+//.getElementById("title")
+//.getElementById("title").innerHTML = "GoodBye";
+
+//By Query Selector
+// .querySelector("h1")
+// .querySelector("#title")
+
+
+//add CSS selector (pointing to CSS selector)
+// .document.querySelector("h1").classList.add("btn");
+
+// /Change Content
+// document.querySelector("h1").textContent;   Just Text
+//document.querySelector("h1").innerContent;  "<strong>Text</strong>"
+// ALL about CSS should be in "" to considere it as CSS
+
+//Get Attributes
+// .document.querySelector("a").attributes;
+// .document.querySelector("a").getAttribute(href);
+
+//Set Atribute
+// .document.querySelector("a").setAttribute("href", "https:adb.com");
